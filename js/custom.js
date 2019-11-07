b0f70e4ce8577e3379d1b32d263275b776eaa218
Chen Hui Jing <kakyou_tensai@yahoo.com>
2019-11-08 07:41:45 +0800

    Add TTS function

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,19 @@
 ((window, undefined) => {
   const document = window.document;
   const docElement = document.documentElement;
+  const docBody = document.body
 
   const speechRecognition = window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition || window.oSpeechRecognition || window.SpeechRecognition;
   const speechGrammarList =  window.webkitSpeechGrammarList || window.mozSpeechGrammarList || window.msSpeechGrammarList || window.oSpeechGrammarList || window.SpeechGrammarList;
+  const speechSynthesis = window.speechSynthesis;
 
   function addClass(className) {
     docElement.className = `${docElement.className} ${className}`;
   }
 
   const colours = ['maroon', 'darkred', 'brown', 'firebrick', 'rosybrown', 'indianred', 'lightcoral', 'red', 'snow', 'mistyrose', 'salmon', 'tomato', 'darksalmon', 'coral', 'orangered', 'lightsalmon', 'sienna', 'seashell', 'saddlebrown', 'chocolate', 'sandybrown', 'peachpuff', 'peru', 'linen', 'bisque', 'darkorange', 'burlywood', 'tan', 'antiquewhite', 'navajowhite', 'blanchedalmond', 'papayawhip', 'moccasin', 'orange', 'wheat', 'oldlace', 'floralwhite', 'darkgoldenrod', 'goldenrod', 'cornsilk', 'gold', 'khaki', 'lemonchiffon', 'palegoldenrod', 'darkkhaki', 'olive', 'beige', 'lightgoldenrodyellow', 'yellow', 'lightyellow', 'ivory', 'olivedrab', 'yellowgreen', 'darkolivegreen', 'greenyellow', 'chartreuse', 'lawngreen', 'darkseagreen', 'darkgreen', 'green', 'lime', 'forestgreen', 'limegreen', 'lightgreen', 'palegreen', 'honeydew', 'seagreen', 'mediumseagreen', 'springgreen', 'mintcream', 'mediumspringgreen', 'mediumaquamarine', 'aquamarine', 'turquoise', 'lightseagreen', 'mediumturquoise', 'teal', 'darkcyan', 'aqua', 'cyan', 'darkslategray', 'darkslategrey', 'paleturquoise', 'lightcyan', 'azure', 'darkturquoise', 'cadetblue', 'powderblue', 'lightblue', 'deepskyblue', 'skyblue', 'lightskyblue', 'steelblue', 'aliceblue', 'dodgerblue', 'slategray', 'slategrey', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'cornflowerblue', 'royalblue', 'navy', 'darkblue', 'mediumblue', 'blue', 'midnightblue', 'lavender', 'ghostwhite', 'slateblue', 'darkslateblue', 'mediumslateblue', 'mediumpurple', 'rebeccapurple', 'blueviolet', 'indigo', 'darkorchid', 'darkviolet', 'mediumorchid', 'purple', 'darkmagenta', 'thistle', 'plum', 'violet', 'fuchsia', 'magenta', 'orchid', 'mediumvioletred', 'deeppink', 'hotpink', 'lavenderblush', 'palevioletred', 'crimson', 'pink', 'lightpink', 'black', 'dimgray', 'dimgrey', 'gray', 'grey', 'darkgray', 'darkgrey', 'silver', 'lightgray', 'lightgrey', 'gainsboro', 'whitesmoke', 'white'];
-  const grammar = '#JSGF V1.0; grammar colours; public <colour> = ' + colours.join(' | ') + ' ;'
+  const grammar = '#JSGF V1.0; grammar colours; public <colour> = ' + colours.join(' | ') + ' ;';
+  let voices = [];
 
   function detectSpeech() {
     const recognition = new speechRecognition();
@@ -24,7 +27,6 @@
 
     const micBtn = document.getElementById('activateMic')
     const consoleLog = document.getElementById('consoleLog')
-    const docBody = document.body
 
     micBtn.addEventListener('click', function() {
       recognition.start();
@@ -37,6 +39,7 @@
       const sanitiseColour = colour.replace(/\s/g, '');
 
       consoleLog.innerHTML = 'You probably said: ' + sanitiseColour + '.\nConfidence: ' + event.results[0][0].confidence;
+      readResponse('You probably said: ' + colour);
       docBody.style.setProperty('--bg-colour', sanitiseColour);
     }
 
@@ -53,10 +56,48 @@
     }
   }
 
+  function readResponse(result) {
+    docBody.style.setProperty('--display', 'block');
+    populateVoiceList();
+    // speechSynthesis.addEventListener('voiceschanged', function() {
+    //   populateVoiceList();
+    // });
+
+    const responseForm = document.getElementById('hearResponse')
+    responseForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const select = document.getElementById('pickVoice');
+      const utterStuff = new SpeechSynthesisUtterance(result);
+      const selectedVoice = select.selectedOptions[0].getAttribute('data-name');
+      voices.forEach(function(voice) { 
+        if(voice.name === selectedVoice) {
+          utterStuff.voice = voice;
+        }
+      });
+      speechSynthesis.speak(utterStuff);
+    }, false)
+  }
+
+  function populateVoiceList() {
+    const select = document.getElementById('pickVoice');
+    voices = speechSynthesis.getVoices();
+    voices.forEach(function(voice) { 
+      const option = document.createElement('option');
+      option.textContent = voice.name + ' (' + voice.lang + ')';
+      if(voice.default) {
+        option.textContent += ' -- DEFAULT';
+      }
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      select.appendChild(option);
+    });
+  }
+
   docElement.className = docElement.className.replace(/(^|\s)no-js(\s|$)/, '$1js$2');
   if (speechRecognition !== undefined) {
     addClass('speech');
     detectSpeech();
+    
   } else {
     addClass('no-speech');
   }
--- scripts.js
+++ scripts.js
@@ -3,8 +3,10 @@
 (function (window, undefined) {
   var document = window.document;
   var docElement = document.documentElement;
+  var docBody = document.body;
   var speechRecognition = window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition || window.oSpeechRecognition || window.SpeechRecognition;
   var speechGrammarList = window.webkitSpeechGrammarList || window.mozSpeechGrammarList || window.msSpeechGrammarList || window.oSpeechGrammarList || window.SpeechGrammarList;
+  var speechSynthesis = window.speechSynthesis;
 
   function addClass(className) {
     docElement.className = "".concat(docElement.className, " ").concat(className);
@@ -12,6 +14,7 @@
 
   var colours = ['maroon', 'darkred', 'brown', 'firebrick', 'rosybrown', 'indianred', 'lightcoral', 'red', 'snow', 'mistyrose', 'salmon', 'tomato', 'darksalmon', 'coral', 'orangered', 'lightsalmon', 'sienna', 'seashell', 'saddlebrown', 'chocolate', 'sandybrown', 'peachpuff', 'peru', 'linen', 'bisque', 'darkorange', 'burlywood', 'tan', 'antiquewhite', 'navajowhite', 'blanchedalmond', 'papayawhip', 'moccasin', 'orange', 'wheat', 'oldlace', 'floralwhite', 'darkgoldenrod', 'goldenrod', 'cornsilk', 'gold', 'khaki', 'lemonchiffon', 'palegoldenrod', 'darkkhaki', 'olive', 'beige', 'lightgoldenrodyellow', 'yellow', 'lightyellow', 'ivory', 'olivedrab', 'yellowgreen', 'darkolivegreen', 'greenyellow', 'chartreuse', 'lawngreen', 'darkseagreen', 'darkgreen', 'green', 'lime', 'forestgreen', 'limegreen', 'lightgreen', 'palegreen', 'honeydew', 'seagreen', 'mediumseagreen', 'springgreen', 'mintcream', 'mediumspringgreen', 'mediumaquamarine', 'aquamarine', 'turquoise', 'lightseagreen', 'mediumturquoise', 'teal', 'darkcyan', 'aqua', 'cyan', 'darkslategray', 'darkslategrey', 'paleturquoise', 'lightcyan', 'azure', 'darkturquoise', 'cadetblue', 'powderblue', 'lightblue', 'deepskyblue', 'skyblue', 'lightskyblue', 'steelblue', 'aliceblue', 'dodgerblue', 'slategray', 'slategrey', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'cornflowerblue', 'royalblue', 'navy', 'darkblue', 'mediumblue', 'blue', 'midnightblue', 'lavender', 'ghostwhite', 'slateblue', 'darkslateblue', 'mediumslateblue', 'mediumpurple', 'rebeccapurple', 'blueviolet', 'indigo', 'darkorchid', 'darkviolet', 'mediumorchid', 'purple', 'darkmagenta', 'thistle', 'plum', 'violet', 'fuchsia', 'magenta', 'orchid', 'mediumvioletred', 'deeppink', 'hotpink', 'lavenderblush', 'palevioletred', 'crimson', 'pink', 'lightpink', 'black', 'dimgray', 'dimgrey', 'gray', 'grey', 'darkgray', 'darkgrey', 'silver', 'lightgray', 'lightgrey', 'gainsboro', 'whitesmoke', 'white'];
   var grammar = '#JSGF V1.0; grammar colours; public <colour> = ' + colours.join(' | ') + ' ;';
+  var voices = [];
 
   function detectSpeech() {
     var recognition = new speechRecognition();
@@ -24,7 +27,6 @@
     recognition.maxAlternatives = 1;
     var micBtn = document.getElementById('activateMic');
     var consoleLog = document.getElementById('consoleLog');
-    var docBody = document.body;
     micBtn.addEventListener('click', function () {
       recognition.start();
       consoleLog.innerHTML = 'Ready to receive a colour command.';
@@ -35,6 +37,7 @@
       var colour = event.results[last][0].transcript;
       var sanitiseColour = colour.replace(/\s/g, '');
       consoleLog.innerHTML = 'You probably said: ' + sanitiseColour + '.\nConfidence: ' + event.results[0][0].confidence;
+      readResponse('You probably said: ' + colour);
       docBody.style.setProperty('--bg-colour', sanitiseColour);
     };
 
@@ -49,6 +52,44 @@
     recognition.onerror = function (event) {
       consoleLog.innerHTML = 'Recognition error: ' + event.error;
     };
+  }
+
+  function readResponse(result) {
+    docBody.style.setProperty('--display', 'block');
+    populateVoiceList(); // speechSynthesis.addEventListener('voiceschanged', function() {
+    //   populateVoiceList();
+    // });
+
+    var responseForm = document.getElementById('hearResponse');
+    responseForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+      var select = document.getElementById('pickVoice');
+      var utterStuff = new SpeechSynthesisUtterance(result);
+      var selectedVoice = select.selectedOptions[0].getAttribute('data-name');
+      voices.forEach(function (voice) {
+        if (voice.name === selectedVoice) {
+          utterStuff.voice = voice;
+        }
+      });
+      speechSynthesis.speak(utterStuff);
+    }, false);
+  }
+
+  function populateVoiceList() {
+    var select = document.getElementById('pickVoice');
+    voices = speechSynthesis.getVoices();
+    voices.forEach(function (voice) {
+      var option = document.createElement('option');
+      option.textContent = voice.name + ' (' + voice.lang + ')';
+
+      if (voice["default"]) {
+        option.textContent += ' -- DEFAULT';
+      }
+
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      select.appendChild(option);
+    });
   }
 
   docElement.className = docElement.className.replace(/(^|\s)no-js(\s|$)/, '$1js$2');
